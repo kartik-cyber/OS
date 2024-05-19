bca3f49231e4f6bb7f5265edc11996963f0ff434
Kartik Bhat <76040124+kartik-cyber@users.noreply.github.com>
2024-05-19 22:15:07 +0530

    program_4

---
+++ program_4.sh
@@ -0,0 +1,19 @@
+if [ $# -eq 0 ]
+then
+echo "no arguments"
+else
+for file in $*
+do
+if [ -e $file ]
+then
+upper=`echo $file | tr '[a-z]' '[A-Z]'`
+echo "file is converted into :$upper"
+else
+echo "file does not exist"
+fi
+done
+fi
+......................................................... Run: $sh Lab4.sh file1 file2
+Output:
+file is converted into :FILE1
+file is converted into :FILE2
